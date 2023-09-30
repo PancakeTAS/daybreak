c4ae939882c74e96530e4f816e2d09ba6fb3c8f2
PancakeTAS <pancake@mgnet.work>
2023-10-01 00:49:42 +0200

    Create plugin files

---
+++ src/main/java/de/pancake/daybreak/DaybreakPlugin.java
@@ -0,0 +1,11 @@
+package de.pancake.daybreak;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main class of the plugin.
+ * @author Pancake
+ */
+public class DaybreakPlugin extends JavaPlugin {
+
+}

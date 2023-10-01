466598cfa8e602a3babad2a4c0cc7e68e57e15f4
PancakeTAS <pancake@mgnet.work>
2023-10-01 02:55:51 +0200

    Implement daybreak plugin base

--- src/main/java/de/pancake/daybreak/DaybreakPlugin.java
+++ src/main/java/de/pancake/daybreak/DaybreakPlugin.java
@@ -1,11 +1,79 @@
 package de.pancake.daybreak;
 
+import de.pancake.daybreak.commands.DaybreakCommand;
+import de.pancake.daybreak.listeners.SurvivalListener;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Main class of the plugin.
  * @author Pancake
  */
-public class DaybreakPlugin extends JavaPlugin {
+public class DaybreakPlugin extends JavaPlugin implements Listener {
+
+    /** Size of the border */
+    public static final int BORDER_SIZE = 1000;
+    /** Today's date */
+    public static final long TODAY = ChronoUnit.DAYS.between(LocalDate.ofEpochDay(0), LocalDateTime.now(Clock.systemUTC()));
+
+    /** List of survivors */
+    private final List<UUID> survivors = new ArrayList<>();
+
+    /**
+     * Load daybreak plugin
+     */
+    @Override
+    public void onEnable() {
+        Bukkit.getCommandMap().register("daybreak", "db", new DaybreakCommand(this));
+        Bukkit.getPluginManager().registerEvents(new SurvivalListener(this), this);
+    }
+
+    public void reset() {
+
+    }
+
+    public void revive(UUID uniqueId) {
+
+    }
+
+    public void kill(UUID uniqueId) {
+
+    }
+
+    // Query survivors list
+
+    /**
+     * Check survivors list for a given player.
+     * @param uniqueId The unique id of the player.
+     * @return True if the player is a survivor, false otherwise.
+     */
+    public boolean isSurvivor(UUID uniqueId) {
+        return this.survivors.contains(uniqueId);
+    }
+
+    /**
+     * Add a player to the survivors list.
+     * @param uniqueId The unique id of the player.
+     */
+    public void addSurvivor(UUID uniqueId) {
+        this.survivors.add(uniqueId);
+    }
+
+    /**
+     * Remove a player from the survivors list.
+     * @param uniqueId The unique id of the player.
+     */
+    public void removeSurvivor(UUID uniqueId) {
+        this.survivors.remove(uniqueId);
+    }
 
 }

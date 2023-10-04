476cc9bb11c5e43805b43c9c7727f09f351fec0e
PancakeTAS <pancake@mgnet.work>
2023-10-05 00:05:25 +0200

    Create request builder

---
+++ src/main/java/de/pancake/daybreak/webhook/data/Author.java
@@ -0,0 +1,21 @@
+package de.pancake.daybreak.webhook.data;
+
+import lombok.Builder;
+
+/**
+ * Author of the webhook.
+ * @author Pancake
+ */
+@Builder
+public class Author {
+
+    /** Name of the author. */
+    public String name;
+
+    /** Url of the author icon. */
+    public String icon_url;
+
+    /** Link of the author. */
+    public String url;
+
+}

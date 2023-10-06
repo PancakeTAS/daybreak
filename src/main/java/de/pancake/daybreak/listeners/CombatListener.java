42196da5c10166a53e96180857bc2e2b367a3cc1
PancakeTAS <pancake@mgnet.work>
2023-10-06 20:19:58 +0200

    Rework combat logging system

--- src/main/java/de/pancake/daybreak/listeners/CombatListener.java
+++ src/main/java/de/pancake/daybreak/listeners/CombatListener.java
@@ -5,12 +5,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+import static de.pancake.daybreak.listeners.SurvivalListener.PREFIX;
 import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
 
 /**
@@ -19,8 +21,14 @@ import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
  */
 public class CombatListener implements Listener {
 
-    /** Combat logging tasks */
+    /** Players currently in combat */
     private final Map<Player, Integer> timers = new HashMap<>();
+
+    /** Players that will be kicked and are allowed to disconnect without punishment */
+    private final List<Player> safe = new ArrayList<>();
+
+    /** Players that will get punished on next login */
+    private final List<UUID> unsafe = new ArrayList<>();
 
     /**
      * Initialize combat listener.
@@ -45,13 +53,26 @@ public class CombatListener implements Listener {
     }
 
     /**
+     * Handle player join event.
+     * @param e Player join event.
+     */
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        var p = e.getPlayer();
+        if (this.unsafe.remove(p.getUniqueId())) {
+            p.sendMessage(miniMessage().deserialize("<prefix><red>You logged off without /dc. Your health has been lowered 3 hearts and you will not be given invincibility frames</red>", PREFIX));
+            p.setNoDamageTicks(5);
+        }
+    }
+
+    /**
      * Handle player damage event.
      * @param e Player damage event.
      */
     @EventHandler
-    public void onDamage(EntityDamageEvent e) {
-        if (e.getEntity() instanceof Player p)
-            this.timers.put(p, 10);
+    public void onDamage(EntityDamageByEntityEvent e) {
+        if (e.getEntity() instanceof Player p && e.getDamager() instanceof Player)
+            this.timers.put(p, 30);
     }
 
     /**
@@ -64,6 +85,11 @@ public class CombatListener implements Listener {
         if (this.timers.containsKey(p) && !p.isDead()) {
             p.setLastDamageCause(new EntityDamageEvent(p, EntityDamageEvent.DamageCause.CUSTOM, 1000));
             p.setHealth(0);
+        } else if (!this.safe.remove(p)) {
+            if (p.getHealth() > 6.0)
+                p.setHealth(6.0);
+
+            this.unsafe.add(p.getUniqueId());
         }
     }
 

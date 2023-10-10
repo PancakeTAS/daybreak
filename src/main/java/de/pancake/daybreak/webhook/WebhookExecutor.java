7cc6c241f324352cd958cfffc0fdc3235f6e91bb
PancakeTAS <pancake@mgnet.work>
2023-10-10 20:16:45 +0200

    Update webhook with crown information

--- src/main/java/de/pancake/daybreak/webhook/WebhookExecutor.java
+++ src/main/java/de/pancake/daybreak/webhook/WebhookExecutor.java
@@ -2,6 +2,7 @@ package de.pancake.daybreak.webhook;
 
 import com.google.gson.Gson;
 import de.pancake.daybreak.DaybreakPlugin;
+import de.pancake.daybreak.crowns.Crown;
 import de.pancake.daybreak.webhook.data.Embed;
 import de.pancake.daybreak.webhook.data.Field;
 import de.pancake.daybreak.webhook.data.Footer;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 import static de.pancake.daybreak.DaybreakBootstrap.LAST_DEATHS;
 import static de.pancake.daybreak.DaybreakPlugin.HEADS_KEY;
 import static de.pancake.daybreak.DaybreakPlugin.HEADS_TYPE;
+import static de.pancake.daybreak.webhook.WebhookUtil.getPlayerName;
 
 /**
  * This class is used to execute the webhooks.
@@ -37,9 +39,10 @@ public class WebhookExecutor {
      * Send death message to webhook.
      * @param p Player that died
      * @param k Killer or null
+     * @param c Crown or null
      * @param msg Death message
      */
-    public void sendDeathMessage(Player p, Player k, String msg) {
+    public void sendDeathMessage(Player p, Player k, Crown c, String msg) {
         var embed = Embed.builder();
 
         // grab player head information
@@ -62,11 +65,17 @@ public class WebhookExecutor {
             .color(0x9F23EB)
             .thumbnail(Image.builder().url("https://crafatar.com/avatars/" + p.getUniqueId() + "?overlay").build());
 
+
         // add killer footer
         if (k != null)
-            embed.footer(Footer.builder()
-                .text("Murdered by " + k.getName())
-                .icon_url("https://crafatar.com/avatars/" + k.getUniqueId() + "?overlay").build());
+            if (c == null)
+                embed.footer(Footer.builder()
+                    .text("Murdered by " + k.getName())
+                    .icon_url("https://crafatar.com/avatars/" + k.getUniqueId() + "?overlay").build());
+            else
+                embed.footer(Footer.builder()
+                    .text(k.getName() + " stole the " + c.getType().getName() + " and is now rank " + (c.getType().ordinal()+1) + ".")
+                    .icon_url("https://crafatar.com/avatars/" + k.getUniqueId() + "?overlay").build());
 
         // send webhook
         WebhookUtil.send(null, embed.build());
@@ -79,11 +88,23 @@ public class WebhookExecutor {
     public void sendResetMessage(DaybreakPlugin plugin) {
         var embed = Embed.builder();
 
+        // get top 3 players
+        var crowns = plugin.crownListener.crowns;
+        var first = crowns[0].getHolder() != null ? getPlayerName(crowns[0].getHolder()) : "Unclaimed";
+        var second = crowns[1].getHolder() != null ? getPlayerName(crowns[1].getHolder()) : "Unclaimed";
+        var third = crowns[2].getHolder() != null ? getPlayerName(crowns[2].getHolder()) : "Unclaimed";
+
         // create embed
         embed.title("Daybreak has reset!")
             .description("It is now 0:00 UTC and the server has finished resetting the world.")
             .thumbnail(Image.builder().url("https://static.planetminecraft.com/files/image/minecraft/texture-pack/2023/003/16537288-pack_l.webp").build()) // FIXME: fix icon
             .fields(new Field[] {
+                    Field.builder()
+                            .name("Leaderboard")
+                            .value("1. " + first + "\n"
+                                    + "2. " + second + "\n"
+                                    + "3. " + third)
+                            .build(),
                     Field.builder()
                             .name("Survivors")
                             .value(plugin.lastSession.stream().map(WebhookUtil::getPlayerName).collect(Collectors.joining(", ")))
